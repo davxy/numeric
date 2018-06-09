ee24890ec31bc39c47cf4bc585e2e74ed23b4324
Davide Galassi <davxy@datawok.net>
2018-06-09 17:42:45 +0200

    Python: machine precision and numerical cancellation example

---
+++ python/machine/cancel.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def p1(x):
+    return (x-1.0)**6
+
+def p2(x):
+    return x**6 - 6*(x**5) + 15*(x**4) - 20*(x**3) + 15*(x**2) - 6*x + 1
+
+xx = np.linspace(0.996, 1.004, 81)
+plt.plot(xx, [ p1(x) for x in xx ], color='blue')
+plt.gcf().text(0.7, 0.9, 'p1', color='blue')
+plt.plot(xx, [ p2(x) for x in xx ], color='orange')
+plt.gcf().text(0.75, 0.9, 'p2', color='orange')
+plt.grid(True)
+plt.show()
