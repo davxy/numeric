66598445f3de0fd9fdc0e902d402515e2fc1b366
Davide Galassi <davxy@datawok.net>
2019-05-11 19:49:37 +0200

    Octave cubic spline code adjustments (formal)

--- octave/spline3.m
+++ octave/spline3.m
@@ -1,4 +1,4 @@
-function yq = spline3(x,y,xq)
+function Y = spline3(x, y, X)
     %
     % Cubic Spline interpolation method.
     %
@@ -11,70 +11,72 @@ function yq = spline3(x,y,xq)
     %
     x = x(:);
     y = y(:);
-    [a,b,c,d] = spline_coef(x,y);
-    nx = length(x);
-    n = length(xq);
-    for i=1:length(xq)
-        yq(i) = spline_eval(a,b,c,d,x,xq(i));
+    [a, b, c, d] = spline3_coef(x, y);
+    for i = 1:length(X)
+        Y(i) = spline3_eval(a, b, c, d, x, X(i));
     end
 end
 
 
-function yq = spline_eval(a,b,c,d,x,xq)
-    n = length(x);
+function Yi = spline3_eval(a, b, c, d, x, Xi)
+    %
+    % Evaluation function.
+    %
+    % Input:
+    %   - [ a, b, c, d ] : spline coefficients list 
+    %   - x  : precomputed points x axis values
+    %   - Xi : query point
+    % Output:
+    %   - Yi : spline value at query point
+    %
     i=1;
-    while (i <= n-1 & xq > x(i+1))
+    while (i <= length(x)-1 & Xi > x(i+1))
         i=i+1;
     end
-    % xq is between x(i) and x(i+1)
-    yq = a(i)*(xq-x(i))^3 + b(i)*(xq-x(i))^2 + c(i)*(xq-x(i)) + d(i);
+    % Xi is between x(i) and x(i+1)
+    Yi = a(i)*(Xi-x(i))^3 + b(i)*(Xi-x(i))^2 + c(i)*(Xi-x(i)) + d(i);
 end
 
 
 function T = build_tridiag(h, n)
+    %
     % Internal helper function to build the trigiagonal matrix used to find
     % the unknown values m(i)
-    for i=1:n
-        T(i,i) = 2*(h(i)+h(i+1));
+    %
+    for i = 1:n
+        T(i, i) = 2*(h(i) + h(i+1));
     end
-    for i=1:n-1
-        T(i+1,i) = h(i);
-        T(i,i+1) = h(i+1);
+    for i = 1:n-1
+        T(i+1, i) = h(i);
+        T(i, i+1) = h(i+1);
     end
 end
 
-function [a,b,c,d] = spline_coef(x,y)
+function [a, b, c, d] = spline3_coef(x, y)
+    %
     % Helper function returning a list of coefficients vectors.
+    % Assumption: x and y shall be of the same length
     %
     % Input:
-	  % - x  : interpolation nodes x axis values
-	  % - y  : interpolation nodes y axis values
+	%   - x : interpolation nodes x axis values
+	%   - y : interpolation nodes y axis values
     % Output:
-	  %   - [a,b,c,d] : List of coefficients vectors
-    % The polynomial used in the interval [x(i), x(i+1)) is constructed as
+	%   - [a,b,c,d] : List of coefficients vectors
+    %     The polynomial used in the interval [x(i), x(i+1)) is constructed as
+	%     Ci(x) = a[i](x-x[i])^3 + bi(x-x[i])^2 + c[i](x-x[i]) + d[i]
     %
-	  %   Ci(x) = a[i](x-x[i])^3 + bi(x-x[i])^2 + c[i](x-x[i]) + d[i]
-    %
-    if (any(size(x) != size(y)))
-        error('input x and y vectors must be column vectors of equal length');
-    end
     n = length(x);
-    % Intervals
+    % Intervals lengths
     h = x(2:n) - x(1:n-1);
     % Divided differences
     dd = (y(2:n) - y(1:n-1))./h;
-    
     % Build the tridiagonal matrix T
     T = build_tridiag(h,n-2);
-
-    % System right hand side
+    % System right hand side and solve the system
     rhs = 6*(dd(2:end)-dd(1:end-1));
-    % Solve the system
     m = T\rhs;
-    % Use natural boundary conditions where second derivative
-    % is zero at the endpoints
+    % Natural boundary conditions: second derivative is zero at the endpoints
     m = [ 0; m; 0];
-
     % Get the coefficients
     d = y;
     c = dd - h.*(2*m(1:end-1) + m(2:end))/6;
--- octave/spline3_test.m
+++ octave/spline3_test.m
@@ -1,28 +1,27 @@
-% TEST: cubic spline interpolation
+% Cubic spline interpolation test
 
 nq = 100;   % query points
-n  = 50;     % interpolation nodes
+n  = 15;    % interpolation nodes
 
-x = linspace(-1,1,n);
-for i=1:n
-    y(i)=1/(1 + 25*x(i)^2);
+x = linspace(-1, 1, n);
+y = zeros(1,n);
+for i = 1:n
+    y(i) = 1/(1 + 25*x(i)^2);
 end
 
 % 100 evenly spaced values between 0 and 100
-xx = linspace(min(x),max(x),100);
-
+X = linspace(min(x), max(x), nq);
 % Get true function values
-for i=1:nq
-    ff(i)=1/(1 + 25*xx(i)^2);
+f = zeros(1,nq);
+for i = 1:nq
+    f(i)=1/(1 + 25*X(i)^2);
 end
+% Get yy values via interpolation
+Y = spline3(x, y, X);
 
-% interpolate to get yy values
-yy = spline3(x,y,xx);
-
-% yy = lagrange(xx,x,y);
 % pre-computed y values are plotted with 'o',
 % interpolated y values are plotted with '.'
-plot(xx,yy,'+',xx,ff,x,y,'o');
+plot(X, Y, '+', X, f, x, y, 'o');
 
 % Wait for input
 pause;
