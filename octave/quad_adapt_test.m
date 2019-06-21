3a9ad9b3832d2e2fefc4644ee2346fd27b34602a
Davide Galassi <davxy@datawok.net>
2019-06-21 17:59:47 +0200

    Octave quadrature tests update

--- octave/quad_adapt_test.m
+++ octave/quad_adapt_test.m
@@ -4,68 +4,66 @@ f = inline('-2*x^(-3) * cos(x^(-2))');
 a = 0.5;
 b = 100;
 Iexp = sin(10^(-4)) - sin(4) % Expected integral value
+set(gca, 'XScale', 'log');
 
-#f = inline('0.2 + 25*x - 200*x^2 + 675*x^3 - 900*x^4 + 400*x^5');
-#a = 0.0;
-#b = 0.8;
-#Iexp = 1.640533    % Expected integral value
+%f = inline('0.2 + 25*x - 200*x^2 + 675*x^3 - 900*x^4 + 400*x^5');
+%a = 0.0;
+%b = 0.8;
+%Iexp = 1.640533    % Expected integral value
 
-N = 10000;    % Number of points for the smooth plot
+tol = 10^(-2)       % Error tolerance
 
-%------------------------------------------------------------------------------
-
+display('-------------------------------------------');
 % Trapezoid adaptive method with relative error
-display('> Trapezoid adaptive method');
-[ I x ] = quad_trapezoid_adapt(a, b, f, 10^(-4));
-n = length(x)
-I
-E = abs(Iexp - I)/Iexp   % relative error
+[ I x ] = quad_trapezoid_adapt(a, b, f, tol);
+E = abs(Iexp - I)/Iexp;   % relative error
+n = length(x);
+printf('> Trapezoid adaptive method\n I = %f\n E = %f\n n = %d\n', I, E, n);
 
 % Trapezoid method with relative error
-display('> Trapezoid method');
-I = quad_trapezoid(a, b, f, n)
-E = abs(Iexp - I)/Iexp   % relative error
+I = quad_trapezoid(a, b, f, n-1);
+E = abs(Iexp - I)/Iexp;   % relative error
+printf('> Trapezoid method\n I = %f\n E = %f\n n = %d\n', I, E, n);
 
-% 1/3 Simpson's method with relative error
-display('> Simpson method');
-I = quad_simpson(a, b, f, n)
-E = abs(Iexp - I)/Iexp
+% Simpson's method with relative error
+I = quad_simpson(a, b, f, n-1);
+E = abs(Iexp - I)/Iexp;
+printf('> Simpson method\n I = %f\n E = %f\n n = %d\n', I, E, n);
 
-%------------------------------------------------------------------------------
-
+display('-------------------------------------------');
 % Simpson adaptive method with relative error
-display('> Simpson adaptive method');
 [ I x ] = quad_simpson_adapt(a, b, f, 10^(-4));
-n = length(x)
-I
-E = abs(Iexp - I)/Iexp   % relative error
+E = abs(Iexp - I)/Iexp;   % relative error
+n = length(x);
+printf('> Simpson adaptive method\n I = %f\n E = %f\n n = %d\n', I, E, n);
 
 % Trapezoid method with relative error
-display('> Trapezoid method');
-I = quad_trapezoid(a, b, f, n)
-E = abs(Iexp - I)/Iexp   % relative error
+I = quad_trapezoid(a, b, f, n-1);
+E = abs(Iexp - I)/Iexp;   % relative error
+printf('> Trapezoid method\n I = %f\n E = %f\n n = %d\n', I, E, n);
 
-% 1/3 Simpson's method with relative error
-display('> Simpson method');
-I = quad_simpson(a, b, f, n)
-E = abs(Iexp - I)/Iexp
+% Simpson's method with relative error
+I = quad_simpson(a, b, f, n-1);
+E = abs(Iexp - I)/Iexp;
+printf('> Simpson method\n I = %f\n E = %f\n n = %d\n', I, E, n);
 
 
-#set(gca, 'XScale', 'log');
+% Plot function graph using the last Simpson nodes
+N = 10000;    % Number of points for a smooth plot
 hold on
 X = linspace(a, b, N);
 Y = zeros(1, N);
 for i = 1:N
     Y(i) = f(X(i));
-endfor
+end
 plot(X, Y, 'b');
 y = zeros(1,n);
 for i = 1:n
     y(i) = f(x(i));
-#    plot([x(i) x(i)], [0 y(i)], 'k');
-endfor
+    %plot([x(i) x(i)], [0 y(i)], 'k');
+end
 plot(x, y, '+k')
 hold off
 
 % Wait for input
-%pause;
+pause;
--- octave/quad_test.m
+++ octave/quad_test.m
@@ -1,37 +1,48 @@
 % TEST: Numeric Integration using Trapezoidal and 1/3 Simpson methods
 
 f = inline('0.2 + 25*x - 200*x^2 + 675*x^3 - 900*x^4 + 400*x^5');
-
 a = 0.0;
 b = 0.8;
-N = 100;    % Number of points for the smooth plot
-n = 10;      % Integration points (segments = n-1)
+Iexp = 1.640533  % Expected integral value
 
-Iexp = 1.640533    % Expected integral value
+%f = inline('sin(x)');
+%a = 0.0;
+%b = pi;
+%Iexp = -cos(b) + cos(a)  % Expected integral value
 
+% n: the number of integration nodes (segments = n-1)
+for n = 2:2:10
+    display('-----------------------------------');
+    printf('n = %d\n', n);
+
+    % Trapezoid method with relative error
+    I = quad_trapezoid(a, b, f, n-1);
+    E = abs(Iexp - I)/Iexp;
+    printf('> Trapezoid I=%f, E=%f\n', I, E);
+
+    % Simpson's method with relative error
+    I = quad_simpson(a, b, f, n-1);
+    E = abs(Iexp - I)/Iexp;
+    printf('> Simpson   I=%f, E=%f\n', I, E);
+end
+
+% Plot using the last aproximation number of nodes
+N = 1000;    % Number of points for the smooth plot
 hold on
 X = linspace(a, b, N);
 Y = zeros(1, N);
 for i = 1:N
     Y(i) = f(X(i));
 endfor
-%plot(X, Y, 'b');
+plot(X, Y, 'b');
 X = linspace(a, b, n);
 Y = zeros(1,n);
 for i = 1:n
     Y(i) = f(X(i));
-    %plot([X(i) X(i)], [0 Y(i)], '--k');
+    plot([X(i) X(i)], [0 Y(i)], '--k');
 endfor
-%plot(X, Y, '--k')
+plot(X, Y, '--k')
 hold off
-
-% Trapezoid method with relative error
-I = quad_trapezoid(a, b, f, n-1)
-E = abs(Iexp - I)/Iexp   % relative error
-
-% 1/3 Simpson's method with relative error
-I = quad_simpson(a, b, f, n-1)
-E = abs(Iexp - I)/Iexp
 
 % Wait for input
 pause;
